08673de7d1928773a85a3ccd2b2534d23669d513
Brooks Becton <bbecton@babelstreet.com>
2019-10-05 09:11:00 -0400

    Init Commit :fishing_pole_and_fish:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 const App: React.FC = () => {
+  const defaultCountValue = 0;
+  const [count, setCount] = useState(defaultCountValue);
+
+  const handleIncrement = (currentCount: number) => {
+    setCount(currentCount + 1);
+  };
+  const handleDecrement = (currentCount: number) => {
+    setCount(currentCount - 1);
+  };
+
+  const handleReset = () => {
+    setCount(defaultCountValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        height: "50%",
+        paddingTop: "20%",
+      }}
+    >
+      <label>
+        Count:
+        <input value={count} type="number" />
+      </label>
+      <button onClick={() => handleIncrement(count)}>+</button>
+      <button onClick={() => handleDecrement(count)}>-</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
-}
+};
 
 export default App;
